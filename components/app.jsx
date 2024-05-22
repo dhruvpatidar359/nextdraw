bf65165bcc10ba7352c09357ce718ae6ce6c35cf
Sarthak Mishra <124512392+sarthakmishra459@users.noreply.github.com>
2024-05-22 13:18:41 +0530

    close fix #24

--- components/app.jsx
+++ components/app.jsx
@@ -45,7 +45,7 @@ const App = () => {
     <div >
 
       <Topbar></Topbar>
-      {isOpen && (selectedElemenet != null || tool != 'selection') && <PropertiesBar />} //Fixed Close Button
+      {isOpen && (selectedElemenet != null || tool != 'selection') && <PropertiesBar />}
       <Canvas ></Canvas>
 
       {open ? <ExportDialog open={open} changeOpen={setOpen} /> : null}
